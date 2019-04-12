7a5bc4d7d71273ffdd7b8009f36ec488a2ffefef
lukasz_brainhub <lukasz@brainhub.pl>
2019-04-12 18:40:38 +0200

    Added more prometheus metrics

--- src/services/prometheusService.js
+++ src/services/prometheusService.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import Prometheus from 'prom-client';
 
 import { LIST_OF_METRICS } from './sonarQubeService';
@@ -8,52 +9,52 @@ const types = {
 
 const metricTypes = {
   // Issues
-  'critical_violations': types.GAUGE,
-  'new_critical_violations': types.GAUGE,
-  'major_violations': types.GAUGE,
-  'new_major_violations': types.GAUGE,
-  'minor_violations': types.GAUGE,
-  'new_minor_violations': types.GAUGE,
+  critical_violations: types.GAUGE,
+  new_critical_violations: types.GAUGE,
+  major_violations: types.GAUGE,
+  new_major_violations: types.GAUGE,
+  minor_violations: types.GAUGE,
+  new_minor_violations: types.GAUGE,
 
-//   // Reliability
-//   'bugs',
-//   'new_bugs',
+  // Reliability
+  bugs: types.GAUGE,
+  new_bugs: types.GAUGE,
 
-//   // maintainability
-//   'sqale_rating', // Maintainability rating
-//   'new_maintainability_rating',
-//   'new_technical_debt',
-//   'code_smells',
-//   'new_code_smells',
+  // maintainability
+  sqale_rating: types.GAUGE, // Maintainability rating
+  new_maintainability_rating: types.GAUGE,
+  new_technical_debt: types.GAUGE,
+  code_smells: types.GAUGE,
+  new_code_smells: types.GAUGE,
 
-//   // complexities:
-//   'cognitive_complexity',
+  // complexities:
+  cognitive_complexity: types.GAUGE,
 
-//   'function_complexity',
-//   'file_complexity',
-//   'class_complexity',
-//   'complexity_in_functions',
-//   'complexity_in_classes',
+  function_complexity: types.GAUGE,
+  file_complexity: types.GAUGE,
+  class_complexity: types.GAUGE,
+  complexity_in_functions: types.GAUGE,
+  complexity_in_classes: types.GAUGE,
 
-//   // size:
-//   'new_lines',
-//   'comment_lines',
-//   'commented_out_code_lines',
-//   'comment_lines_density',
-//   'comment_lines_data',
+  // size:
+  new_lines: types.GAUGE,
+  comment_lines: types.GAUGE,
+  commented_out_code_lines: types.GAUGE,
+  comment_lines_density: types.GAUGE,
+  comment_lines_data: types.GAUGE,
 
-//   // coverage
-//   'overall_coverage',
-//   'new_overall_coverage',
-//   'overall_branch_coverage',
-//   'new_overall_branch_coverage',
-//   'overall_line_coverage',
+  // coverage
+  overall_coverage: types.GAUGE,
+  new_overall_coverage: types.GAUGE,
+  overall_branch_coverage: types.GAUGE,
+  new_overall_branch_coverage: types.GAUGE,
+  overall_line_coverage: types.GAUGE,
 
-//   // Duplications
-//   'duplicated_lines',
-//   'new_duplicated_lines',
-//   'duplicated_lines_density',
-//   'new_duplicated_lines_density',
+  // Duplications
+  duplicated_lines: types.GAUGE,
+  new_duplicated_lines: types.GAUGE,
+  duplicated_lines_density: types.GAUGE,
+  new_duplicated_lines_density: types.GAUGE,
 };
 
 const PrometheusService = () => {
@@ -61,7 +62,7 @@ const PrometheusService = () => {
     const type = metricTypes[metric];
     switch (type) {
       case types.GAUGE:
-        result[metric] = new Prometheus.Gauge({ name: metric, help: metric, labelNames: ['project'] });
+        result[metric] = new Prometheus.Gauge({ name: `sonarQube_${metric}`, help: metric, labelNames: ['project'] });
         break;
       default:
         break;
@@ -75,7 +76,7 @@ const PrometheusService = () => {
       Object.keys(PrometheusMetrics).forEach(metric => {
         switch (metricTypes[metric]) {
           case types.GAUGE:
-            PrometheusMetrics[metric].set({ project }, data[metric].value ? parseInt(data[metric].value) : 0);
+            PrometheusMetrics[metric].set({ project }, data[metric].value ? parseFloat(data[metric].value) : 0);
             break;
           default:
             break;
